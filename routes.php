75f9d80446799580d5d493bcfb319d16e5a47ec8
MahmoudDabbous <mahmoud19728@gmail.com>
2023-03-04 02:03:47 +0200

    Removes routes from router file

---
+++ routes.php
@@ -0,0 +1,12 @@
+<?php
+
+return [
+    '/' => base_path() . 'controllers/home.php',
+    '/about' => base_path() . 'controllers/about.php',
+    '/contact' => base_path() . 'controllers/contact.php',
+
+    '/posts' => base_path() . 'controllers/posts/index.php',
+    '/post' => base_path() . 'controllers/posts/show.php',
+    '/posts/create' => base_path() . 'controllers/posts/create.php',
+    '/posts/store' => base_path() . 'controllers/posts/store.php',
+];
--- src/router.php
+++ src/router.php
@@ -1,14 +1,4 @@
 <?php
 
-$routes = [
-  '/' => base_path() . 'controllers/home.php',
-  '/about' => base_path() . 'controllers/about.php',
-  '/contact' => base_path() . 'controllers/contact.php',
-  
-  '/posts' => base_path() . 'controllers/posts/index.php',
-  '/post' => base_path() . 'controllers/posts/show.php',
-  '/posts/create' => base_path() . 'controllers/posts/create.php',
-  '/posts/store' => base_path() . 'controllers/posts/store.php',
-];
-
+$routes = require_once base_path() . 'routes.php';
 mapRoute($_SERVER['REQUEST_URI'], $routes);
